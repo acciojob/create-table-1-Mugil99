0f54f11c433c52d16f6ed8ff9ffef418860cbc7a
Mugil99 <124775550+Mugil99@users.noreply.github.com>
2023-04-28 21:59:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function insert_Row() {
     //Write your code here
   
+    //Write your code here
+  function insertRow() {
+  const table = document.getElementById("myTable");
+  const row = table.insertRow(); // insert new row at position 1 (just below header)
+
+  const cell1 = row.insertCell();
+  const cell2 = row.insertCell();
+
+  cell1.innerHTML = "Row3 cell1";
+  cell2.innerHTML = "Row3 cell2";
+}
+
+  
+
   
 }
